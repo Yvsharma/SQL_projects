8a2c1eb5a06772a9ac1ea4976e1f4d46520e8c31
Yashvardhan sharma <95120981+Yvsharma@users.noreply.github.com>
2023-01-16 17:31:54 +0530

    Add files via upload

---
+++ project1 Data and questions.sql
@@ -0,0 +1,44 @@
+-- create database project1;
+-- use  project1
+/* table name Employee
+column name and data are as follows
+EmpNo | Ename | Salary | Bonus | DeptId |
+| 102 | Kritika | 60000 | 123 | D01 |
+ | 110 | Tanya | 50000 | 467 | D05 |
+| 106 | Sanya | 48000 | 695 | D02 |
+ | 103 | Shabbir | 45000 | 566 | D01 |
+ | 109 | Daribha | 42000 | NULL | D04 |
+ | 105 | Joseph | 34000 | 875 | D03 |
+| 108 | Nachaobi | 29000 | NULL | D05 | |
+ 104 | Gurpreet | 19000 | 565 | D04 |
+ | 107 | Vergese | 15000 | NULL | D01 |
+ | 101 | Aaliya | 10000 | 234 | D02 |
+ 
+  find the answers for below questions
+ Q1. Find all those employees who have not been given a bonus.
+Q2. Display details of all those employees whose name starts with 'K'
+Q3 Display details of all those employees whose name ends with 'a'
+Q4. Display details of all those employees whose names consist of exactly 5 letters and starts with any letter but has ‘ANYA’ after that.
+Q5. Display names of all the employees containing 'se' as a substring in name
+Q6. Display names of all employees containing 'a' as the second character
+ 
+
+ 
+ 
+ /* table name sales
+ column name and data are as follows
+ Table Sales
+|InvoiceNo|CarId|CustId| SaleDate | PaymentMode |EmpID| SalePrice|Commission|
+ |I00001 |D001 |C0001 |2019-01-24| Credit Card | E004| 613247.00| 73589.64 |
+ |I00002 |S001 |C0002 |2018-12-12| Online | E001| 590321.00| 70838.52 |
+ |I00003 |S002 |C0004 |2019-01-25| Cheque | E010| 604000.00| 72480.00 |
+ |I00004 |D002 |C0001 |2018-10-15| Bank Finance | E007| 659982.00| 79197.84 |
+|I00005 |E001 |C0003 |2018-12-20| Credit Card | E002| 369310.00| 44317.20 |
+|I00006 |S002 |C0002 |2019-01-30| Bank Finance | E007| 620214.00| 74425.68 |
+
+ find the answers for below questions
+ Q1. Display the number of cars purchased by each customer from the SALES table.
+Q2. Display the customer Id and number of cars purchased if the customer purchased more than 1 car from SALES table
+Q3. Display the number of people in each category of payment mode from the table SALES
+Q4. Display the PaymentMode and number of payments made using that mode more than once.
+
